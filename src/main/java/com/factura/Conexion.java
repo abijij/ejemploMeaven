6b25216f1be9b155241cb23a50e468b3a5572853
Omar Sanchez Santiago <omarto.300@gmail.com>
2022-06-01 07:45:57 -0500

    Configuracion de conexion con base de datos
    - Agrega carpeta de recursos
    - Modifica la clase de conexion.java a Conexion.java
    - Implementa factory en PropertiesReader.java para carga unica.

---
+++ src/main/java/com/factura/Conexion.java
@@ -0,0 +1,29 @@
+package com.factura;
+
+import com.factura.config.PropertiesReader;
+
+import java.io.IOException;
+import java.sql.*;
+import java.util.Properties;
+
+public class Conexion {
+
+    public static Connection getMysqlConnection() throws SQLException {
+        String url;
+        String username;
+        String password;
+        try {
+            Properties properties = PropertiesReader.cargarProperties();
+            url = properties.getProperty("database.url");
+            username = properties.getProperty("database.user");
+            password = properties.getProperty("database.password");
+        } catch (IOException e) {
+            System.out.println("Fallo la carga de los propierties");
+            e.printStackTrace();
+            url = "dbc:mysql://localhost:3333/op";
+            username ="root";
+            password="sasa";
+        }
+        return DriverManager.getConnection(url, username, password);
+    }
+}
--- src/main/java/com/factura/Main.java
+++ src/main/java/com/factura/Main.java
@@ -1,13 +1,35 @@
 package com.factura;
 
+import com.factura.config.PropertiesReader;
+
+import java.io.IOException;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.Scanner;
 
 public class Main {
 
 
     public static void main(String[] args) {
+        System.out.println("Cargando propiedades");
+        try {
+            PropertiesReader.cargarProperties();
+            PropertiesReader.cargarProperties();
+            PropertiesReader.cargarProperties();
+            PropertiesReader.cargarProperties();
+            PropertiesReader.cargarProperties();
+        } catch (IOException e) {
+            System.out.println("Problemas al cargar el propierties");
+            e.printStackTrace();
+        }
 
-
+        System.out.println("Probando conexion con base de datos");
+        try {
+            Connection conexion = Conexion.getMysqlConnection();
+        } catch (SQLException e) {
+            System.out.println("Fallo al intentar conectar con bs");
+            e.printStackTrace();
+        }
 
         Scanner scanner = new Scanner(System.in);
 
@@ -20,7 +42,7 @@ public class Main {
         System.out.println("Ingrese el apellido materno del que esta la factura ");
         String apellidoMFactura = scanner.nextLine();
 
-        String nombreCFactura = nombreFactura +"\t" +apellidoPFactura+ "\t"+apellidoMFactura+"\t";
+        String nombreCFactura = nombreFactura + "\t" + apellidoPFactura + "\t" + apellidoMFactura + "\t";
 
         System.out.println("Ingrese el monto del costo del primer producto");
         double precio1 = scanner.nextDouble();
@@ -28,11 +50,11 @@ public class Main {
         System.out.println("Ingrese el precio del segundo producto");
         double precio2 = scanner.nextDouble();
 
-        double precioTotal = precio1 + precio2 ;
+        double precioTotal = precio1 + precio2;
         double impuesto = precioTotal * 0.19;
         double precioImpuesto = precioTotal + impuesto;
 
-        String factura = "la factura " + nombreCFactura + "con un valor bruto total de " + precioTotal + ", con un impuesto de "+ impuesto + " dando un total de " +precioImpuesto;
+        String factura = "la factura " + nombreCFactura + "con un valor bruto total de " + precioTotal + ", con un impuesto de " + impuesto + " dando un total de " + precioImpuesto;
 
         System.out.println(factura);
 
